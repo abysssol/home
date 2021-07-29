4cc23570d2584bf397d2ba1d17376a5f34fb30f4
Abyss <abyssabuse@pm.me>
2021-07-29 12:46:06 -0400

    Clean up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,9 @@ alias git-config "git --git-dir=$HOME/.home-config --work-tree=$HOME"
 alias git-config-sys "sudo git --git-dir=/sys-config --work-tree=/"
 
 alias clippy-all "cargo clippy -- -D clippy::all -W clippy::pedantic -W clippy::nursery"
-alias clippy-res "cargo clippy -- -W clippy::restriction"
-alias clippy-car "cargo clippy -- -W clippy::cargo"
 
 if test -d $HOME/.cargo/bin/
-        set -xp PATH "$HOME/.cargo/bin"
+    set -xp PATH "$HOME/.cargo/bin"
 end
 
 function fish_greeting
