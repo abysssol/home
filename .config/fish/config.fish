67ffbdf42b142ee7d704d3284e3d36ae8b47747e
Abyss <abyssability@pm.me>
2022-04-03 05:40:03 -0400

    Alias emacsclient as `em`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,20 +2,27 @@ alias ls "exa --group-directories-first"
 alias ll "ls -lF"
 alias la "ll -a"
 alias cl "clear"
-
+alias em "emacsclient -c -a ''"
 alias clippy-all "cargo clippy -- -D clippy::all -W clippy::pedantic -W clippy::nursery"
 
-function term
-    alacritty -e fish -c "$argv" & disown
+
+function fork
+    $argv &>/dev/null & disown
 end
 
+function term
+    fork alacritty -e fish -c "$argv"
+end
+
+
 if test -d $HOME/.cargo/bin/
-    set -xp PATH "$HOME/.cargo/bin"
+    set -xp PATH "$HOME/.cargo/bin/"
 end
 
 if test -d $HOME/.emacs.d/bin/
     set -xp PATH "$HOME/.emacs.d/bin/"
 end
+
 
 alias fish_greeting "neofetch"
 starship init fish | source
