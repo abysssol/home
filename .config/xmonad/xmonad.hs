01681e784dc83ef5dbf16ec477fa71f9badb66b9
Abyss <abyssability@pm.me>
2022-06-02 13:48:33 -0400

    Update xmonad to 0.17 and reformat

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,156 +1,161 @@
-import XMonad
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Actions.WithAll (killAll, sinkAll)
-import XMonad.Config.Desktop
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Layout.ThreeColumns (ThreeCol(ThreeColMid))
-import XMonad.Layout.GridVariants (Grid(Grid))
-import XMonad.Layout.ToggleLayouts (toggleLayouts, ToggleLayout(ToggleLayout))
-import XMonad.Layout.Magnifier (magnifierOff, magnifiercz)
-import XMonad.Layout.NoBorders (smartBorders, noBorders)
-import XMonad.Layout.PerWorkspace (onWorkspace)
-import XMonad.Layout.Renamed (renamed, Rename(Replace))
+import qualified Data.Map                      as M
+import           System.Exit
+import           XMonad
+import           XMonad.Actions.WithAll
+import           XMonad.Config.Desktop
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers
+import           XMonad.Hooks.TaffybarPagerHints
+import           XMonad.Hooks.UrgencyHook
+import           XMonad.Layout.GridVariants
+import           XMonad.Layout.Magnifier
+import qualified XMonad.Layout.Magnifier       as Magnifier
+import qualified XMonad.Layout.MultiToggle     as MT
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.PerWorkspace
+import           XMonad.Layout.Renamed
+import           XMonad.Layout.ThreeColumns
+import           XMonad.Layout.ToggleLayouts
+import qualified XMonad.Layout.ToggleLayouts   as T
+import qualified XMonad.StackSet               as W
+import           XMonad.Util.Cursor
+import           XMonad.Util.WorkspaceCompare
 
-import System.Exit (exitSuccess)
-import System.Taffybar.Support.PagerHints (pagerHints)
 
-import qualified XMonad.Layout.Magnifier as Magnifier
-import qualified Data.Map as M
-import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
-import qualified XMonad.Layout.ToggleLayouts as T
-  ( ToggleLayout(Toggle)
-  , toggleLayouts
-  )
-import qualified XMonad.StackSet as W
+main =
+    xmonad
+        $ docks
+        . setEwmhActivateHook doAskUrgent
+        . ewmh
+        . pagerHints
+        $ myConfig
 
-main = xmonad myConfig
+myConfig = desktopConfig { terminal           = "alacritty"
+                         , modMask            = mod4Mask
+                         , focusFollowsMouse  = True
+                         , workspaces         = myWorkspaces
+                         , keys               = myKeys
+                         , manageHook         = myManageHook
+                         , layoutHook         = myLayoutHook
+                         , borderWidth        = 1
+                         , normalBorderColor  = black
+                         , focusedBorderColor = blue
+                         , startupHook        = myStartupHook
+                         , handleEventHook    = handleEventHook def
+                         }
 
-myConfig = docks $ ewmh $ pagerHints
-  desktopConfig
-    { terminal = "alacritty"
-    , modMask = mod4Mask
-    , focusFollowsMouse = True
-    , workspaces = myWorkspaces
-    , keys = myKeys
-    , manageHook = myManageHook
-    , layoutHook = myLayoutHook
-    , borderWidth = 2
-    , normalBorderColor = black
-    , focusedBorderColor = blue
-    , startupHook = myStartupHook
-    , handleEventHook = handleEventHook def <+> fullscreenEventHook
-    }
-
-emacs = "emacsclient -c -a ''"
+editor = "emacsclient -c -a ''"
 browser = "firefox"
 screenLocker = "slock"
-dmenuSystem = "dmenu-manager ~/.config/xmonad/system.toml"
-dmenuCommon = "dmenu-manager ~/.config/xmonad/common.toml"
-dmenuRun =
-  "dmenu_run -i -p 'run: ' " ++
-  "-fn 'Hack Nerd Font:size=16' " ++
-  "-nb '" ++ base03 ++ "' " ++
-  "-nf '" ++ base0 ++ "' " ++
-  "-sb '" ++ blue ++ "' " ++
-  "-sf '" ++ base03 ++ "'"
+dmenu script = concat ["dmenu-manager ~/.config/xmonad/", script, ".toml"]
+dmenuRun = concat
+    [ "dmenu_run -i -p 'run:' "
+    , "-fn 'Hack Nerd Font:size=16' "
+    , "-nb '"
+    , dark3 -- normal background
+    , "' -nf '"
+    , light0 -- normal foreground
+    , "' -sb '"
+    , blue -- selected background
+    , "' -sf '"
+    , dark3 -- selected foreground
+    , "'"
+    ]
 
-wsKeys = [xK_grave] ++ [xK_1 .. xK_9] ++ [xK_0]
-myWorkspaces = ["bg", "web", "dev", "doc", "steam", "5", "6", "7", "8", "9", "0"]
-wsHidden = ["bg"]
+wsKeys = [xK_1 .. xK_9] ++ [xK_grave]
+myWorkspaces = ["web", "dev", "doc", "steam", "5", "6", "7", "8", "9", "0"]
 
 myStartupHook = do
-  windows $ W.greedyView "web"
-  spawn "xsetroot -cursor_name left_ptr"
-  spawn "~/.fehbg"
-  spawn "pkill taffybar; taffybar"
+    spawn "xsetroot -cursor_name left_ptr"
+    spawn "~/.fehbg"
+    spawn "pkill taffybar; taffybar"
 
 myManageHook = custom <+> manageHook desktopConfig
   where
     custom = composeAll
-      [ className =? "Tor Browser" --> doFloat
-      , className =? "Steam" --> doShift "steam"
-      , isDialog --> doFloat
-      ]
+        [ className =? "Tor Browser" --> doFloat
+        , className =? "Steam" --> doShift "steam"
+        , isDialog --> doFloat
+        ]
 
 myLayoutHook =
-  avoidStruts $
-  toggleLayouts
-    full $
-    onWorkspace "bg" grid $ tall ||| center ||| grid
+    avoidStruts
+        $   smartBorders
+        $   toggleLayouts full
+        $   onWorkspace "bg" grid
+        $   tall
+        ||| center
+        ||| grid
   where
-    tall =
-      renamed [Replace "tall"] $
-      smartBorders $
-      magnifierOff $
-      Tall 1 (1 / 12) (1 / 2)
-    center =
-      renamed [Replace "center"] $
-      smartBorders $
-      magnifierOff $
-      ThreeColMid 1 (1 / 12) (1 / 2)
-    grid =
-      renamed [Replace "grid"] $
-      smartBorders $
-      magnifiercz (5 / 3) $
-      Grid (3 / 2)
-    full =
-      renamed [Replace "full"] $
-      noBorders $
-      Full
+    tall   = renamed [Replace "tall"] $ magnifierOff $ Tall 1 (1 / 12) (1 / 2)
+    center = renamed [Replace "center"] $ magnifierOff $ ThreeColMid
+        1
+        (1 / 12)
+        (1 / 2)
+    grid = renamed [Replace "grid"] $ magnifiercz (5 / 3) $ Grid (3 / 2)
+    full = renamed [Replace "full"] $ Full
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-myKeys conf@(XConfig {XMonad.modMask = modMask}) =
-  M.fromList $
-  [ ((m, xK_n), refresh)
-  , ((m_s, xK_q), io exitSuccess)
-    -- Common Programs
-  , ((m, xK_t), spawn $ XMonad.terminal conf)
-  , ((m_s, xK_z), spawn screenLocker)
-  , ((m, xK_e), spawn emacs)
-  , ((m, xK_b), spawn browser)
-  , ((m, xK_q), spawn dmenuSystem)
-  , ((m_s, xK_r), spawn dmenuCommon)
-  , ((m, xK_r), spawn dmenuRun)
-    -- Window Control
-  , ((m, xK_m), windows W.focusMaster)
-  , ((m, xK_j), windows W.focusDown)
-  , ((m, xK_k), windows W.focusUp)
-  , ((m_s, xK_m), windows W.swapMaster)
-  , ((m_s, xK_j), windows W.swapDown)
-  , ((m_s, xK_k), windows W.swapUp)
-  , ((m_s, xK_c), kill)
-  , ((m_c, xK_c), killAll)
-    -- Layout Control
-  , ((m, xK_Tab), sendMessage NextLayout)
-  , ((m, xK_space), sendMessage ToggleLayout >> sendMessage ToggleStruts)
-  , ((m_c, xK_space), sendMessage ToggleStruts)
-  , ((m_s, xK_space), sendMessage Magnifier.Toggle)
-  , ((m_s, xK_h), sendMessage Shrink)
-  , ((m_s, xK_l), sendMessage Expand)
-  , ((m_c, xK_h), sendMessage $ IncMasterN 1)
-  , ((m_c, xK_l), sendMessage $ IncMasterN (-1))
-  , ((m, xK_f), withFocused float)
-  , ((m_s, xK_f), withFocused $ windows . W.sink)
-  , ((m_c, xK_f), sinkAll)
-  ] ++
-    -- Workspaces
-  [ ((m .|. msk, k), windows $ f i)
-  | (i, k) <- zip (XMonad.workspaces conf) wsKeys
-  , (f, msk) <- [(W.greedyView, 0), (W.shift, s)]
-  ]
+myKeys conf@(XConfig { XMonad.modMask = modMask }) =
+    M.fromList
+        $  [ ((m, xK_z)      , refresh)
+           , ((m_s, xK_q)    , spawn softRestart)
+           , ((m_c_s, xK_q)  , io exitSuccess)
+           -- Common Programs
+           , ((m, xK_t)      , spawn $ XMonad.terminal conf)
+           , ((m_s, xK_z)    , spawn screenLocker)
+           , ((m, xK_e)      , spawn editor)
+           , ((m, xK_b)      , spawn browser)
+           , ((m, xK_q)      , spawn $ dmenu "system")
+           , ((m_s, xK_r)    , spawn $ dmenu "common")
+           , ((m, xK_r)      , spawn dmenuRun)
+           -- Window Control
+           , ((m, xK_m)      , windows W.focusMaster)
+           , ((m, xK_j)      , windows W.focusDown)
+           , ((m, xK_k)      , windows W.focusUp)
+           , ((m_s, xK_m)    , windows W.swapMaster)
+           , ((m_s, xK_j)    , windows W.swapDown)
+           , ((m_s, xK_k)    , windows W.swapUp)
+           , ((m_s, xK_c)    , kill)
+           , ((m_c_s, xK_c)  , killAll)
+           -- Layout Control
+           , ((m, xK_Tab)    , sendMessage NextLayout)
+           , ((m_s, xK_h)    , sendMessage Shrink)
+           , ((m_s, xK_l)    , sendMessage Expand)
+           , ((m_c, xK_h)    , sendMessage $ IncMasterN 1)
+           , ((m_c, xK_l)    , sendMessage $ IncMasterN (-1))
+           , ((m, xK_space)  , sendMessage Magnifier.Toggle)
+           , ((m_s, xK_space), sendMessage ToggleStruts)
+           , ((m, xK_f)      , toggleFullscreen)
+           , ((m_s, xK_f)    , withFocused toggleFloat)
+           , ((m_c_s, xK_f)  , sinkAll)
+           ]
+        ++
+           -- Workspaces
+           [ ((m .|. msk, k), windows $ f i)
+           | (i, k  ) <- zip (XMonad.workspaces conf) wsKeys
+           , (f, msk) <- [(W.greedyView, 0), (W.shift, s)]
+           ]
   where
-    m = modMask
-    s = shiftMask
-    c = controlMask
-    m_s = m .|. s
-    m_c = m .|. c
-    m_c_s = m_c .|. s
+    m                = modMask
+    s                = shiftMask
+    c                = controlMask
+    m_s              = m .|. s
+    m_c              = m .|. c
+    m_c_s            = m_c .|. s
+    softRestart      = "xmonad --recompile; xmonad --restart"
+    toggleFullscreen = sendMessage ToggleLayout >> sendMessage ToggleStruts
+    toggleFloat w = windows
+        (\s -> if M.member w (W.floating s)
+            then W.sink w s
+            else (W.float w (W.RationalRect (1 / 6) (1 / 6) (2 / 3) (2 / 3)) s)
+        )
 
 -- Solarized Colors
-black = base02
-white = base2
+black = dark3
+white = light3
 yellow = "#b58900"
 orange = "#cb4b16"
 red = "#dc322f"
@@ -160,11 +165,11 @@ blue = "#268bd2"
 cyan = "#2aa198"
 green = "#859900"
 
-base03 = "#002b36"
-base02 = "#073642"
-base01 = "#586e75"
-base00 = "#657b83"
-base0 = "#839496"
-base1 = "#93a1a1"
-base2 = "#eee8d5"
-base3 = "#fdf6e3"
+dark3 = "#002b36"
+dark2 = "#073642"
+dark1 = "#586e75"
+dark0 = "#657b83"
+light0 = "#839496"
+light1 = "#93a1a1"
+light2 = "#eee8d5"
+light3 = "#fdf6e3"
