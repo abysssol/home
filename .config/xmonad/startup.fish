694b2728b3a5006ada44c3d5b26869c6aa4b77db
Abysss <abysssol@pm.me>
2022-09-29 06:46:21 -0400

    Fix xmonad startup script

--- .config/xmonad/startup.fish
+++ .config/xmonad/startup.fish
@@ -1,8 +1,10 @@
+#! /usr/bin/env fish
+
 pkill taffybar
-taffybar
+taffybar &
 xsetroot -cursor_name left_ptr
 feh --no-fehbg --bg-max ~/Pictures/xmonad-bg
 
-sleep 0.2 &
+sleep 1 &
 wait $last_pid
 systemctl --user restart nm-applet
