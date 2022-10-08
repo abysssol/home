6d27221aa3919991e968adbdf6197ffdb0461368
Abysss <abysssol@pm.me>
2022-10-08 14:09:23 -0400

    Start unclutter-xfixes to hide mouse cursor

--- .config/xmonad/startup.fish
+++ .config/xmonad/startup.fish
@@ -1,10 +1,13 @@
 #! /usr/bin/env fish
 
-pkill taffybar
-taffybar &
 xsetroot -cursor_name left_ptr
 feh --no-fehbg --bg-max ~/Pictures/xmonad-bg
+pkill unclutter
+unclutter --timeout 3 --jitter 36 &
 
-sleep 1 &
-wait $last_pid
+systemctl --user start status-watcher
+pkill taffybar
+taffybar &
+
+sleep 3
 systemctl --user restart nm-applet
